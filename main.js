e0390bd8b1d62a411f3a22b09dfd25b893acc53c
wlissis souza <souzawlissis@gmail.com>
2023-01-03 21:46:37 -0300

    adicionando tipos e habilidades

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const listPoke = document.querySelector(".list-poke");
 const searchInput = document.querySelector(".search");
 
-const URL = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
+const URL = "https://pokeapi.co/api/v2/pokemon?limit=5&offset=0";
 const nomeURL = "https://pokeapi.co/api/v2/pokemon/";
+
 
 function createElementCard(info) {
   //criando elemento
@@ -10,6 +11,7 @@ function createElementCard(info) {
   li.classList.add("poke");
 
   let contentImg = document.createElement("div");
+  contentImg.style.backgroundImage = "";
   contentImg.classList.add("content-img");
 
   let contentName = document.createElement("div");
@@ -19,6 +21,9 @@ function createElementCard(info) {
 
   let attributes = document.createElement("div");
   attributes.classList.add("attributes");
+
+  let tipo = document.createElement("h4");
+  tipo.classList.add("tipo");
 
   let hp = document.createElement("span");
   let attack = document.createElement("span");
@@ -36,10 +41,16 @@ function createElementCard(info) {
   let img = document.createElement("img");
   img.classList.add("img");
 
-  //informacoes
+  //add img
   img.src = `${info.sprites.versions["generation-v"]["black-white"].animated.front_default}`;
+  //add name
   nomePoke.innerHTML = `${info.name}`;
-
+  //add type
+  tipo.innerHTML =
+    info.types.length > 1
+      ? `${info.types[0].type.name} + ${info.types[1].type.name} `
+      : `${info.types[0].type.name}`;
+  //add attributes
   hp.innerHTML = `${info.stats[0].stat.name}: ${info.stats[0].base_stat}`;
   attack.innerHTML = `${info.stats[1].stat.name}: ${info.stats[1].base_stat}`;
   defense.innerHTML = `${info.stats[2].stat.name}: ${info.stats[2].base_stat}`;
@@ -48,13 +59,22 @@ function createElementCard(info) {
   speed.innerHTML = `${info.stats[5].stat.name}: ${info.stats[5].base_stat}`;
 
   contentImg.appendChild(img);
-
+  attributes.appendChild(tipo);
   attributes.appendChild(hp);
   attributes.appendChild(attack);
   attributes.appendChild(defense);
   attributes.appendChild(especialAttack);
   attributes.appendChild(especialDefense);
   attributes.appendChild(speed);
+
+    //add habilidades
+    info.abilities.map((item,index) => {
+        const habilidade = document.createElement("span");
+        habilidade.classList.add("attribute");
+        habilidade.innerHTML = `Hability-${index+1}: ${item.ability.name}`;
+        attributes.appendChild(habilidade);
+    
+      });
 
   contentName.appendChild(nomePoke);
 
@@ -63,11 +83,9 @@ function createElementCard(info) {
   li.appendChild(attributes);
 
   listPoke.appendChild(li);
+
 }
 
-//data pokemons
-
-///add dados local storage
 fetch(URL)
   .then((response) => response.json())
   .then((data) =>
@@ -77,7 +95,6 @@ fetch(URL)
         .then((data) => createElementCard(data))
         .then(() => document.querySelectorAll(".poke"))
         .then((listaNode) => {
-
           searchInput.addEventListener("input", filterCards);
 
           function filterCards() {
@@ -104,4 +121,4 @@ fetch(URL)
         })
     )
   )
-  .catch(error => console.log(error))
+  .catch((error) => console.log(error));
